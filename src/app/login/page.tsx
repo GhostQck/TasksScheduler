8e198c9322b394c3615be45d2993fe698caa5a3f
GhostQck <ghost3dolphin@gmail.com>
2026-07-30 22:18:16 +0400

    Separated the error block

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,21 @@
+import React from 'react';
+
 import { CircleX } from 'lucide-react';
 
 import { InputLabel } from '@/components/form/input';
 import { Button } from '@/components/ui/button';
+import { HTMLAttributes } from 'react';
 
 export default function Login() {
   return (
-    <main className='flex flex-col min-h-screen w-full justify-center items-center'>
+    <main className='flex flex-col min-h-screen pt-[30vh] w-full justify-start items-center'>
       <section className='flex flex-col w-100 p-4 bg-fg rounded-xl shadow-lg items-center'>
         <h1
           className='
             mb-2 font-extrabold uppercase select-none
             md:text-2xl lg:text-4xl
           '
-        >Log In</h1>
+        >Authorization</h1>
 
         <hr className='h-1 w-full mb-4 bg-hl/40 rounded-full border-0' />
 
@@ -43,11 +46,15 @@ export default function Login() {
           >Log In</Button>
         </form>
       </section>
-
-      <div className='opacity-0 w-100 mt-4 p-4 bg-neg-200/40 shadow-lg rounded-lg ring-2 ring-inset ring-neg/70 text-center text-sm text-neg/70 font-semibold uppercase select-none'>
-        <CircleX strokeWidth={2} className='inline-block mr-2 size-[1.1rem] -translate-y-[0.1rem]'/>
-        Authorization Failed
-      </div>
     </main>
+  );
+}
+
+function ErrorBlock({ children }: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className='w-100 mt-4 p-4 bg-neg-200/40 shadow-lg rounded-xl ring-2 ring-inset ring-neg/70 text-center text-sm text-neg/70 font-semibold uppercase select-none'>
+      <CircleX strokeWidth={2} className='inline-block mr-2 size-[1.1rem] -translate-y-[0.1rem]'/>
+      {children}
+    </div>
   );
 }
