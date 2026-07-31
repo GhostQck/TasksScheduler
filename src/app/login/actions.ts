c490e40fecc1f6984d4978f9675a71682f083826
GhostQck <ghost3dolphin@gmail.com>
2026-07-31 11:21:13 +0400

    Added auth form handling

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -9,3 +9,51 @@ import bcrypt from 'bcryptjs';
 import { createSession } from '@/lib/session';
 import { redirect } from 'next/navigation';
 
+export type AuthFormState = {
+  error?: string;
+};
+
+export const authUser = async (
+  prevState: AuthFormState,
+  formData: FormData
+): Promise<AuthFormState> => {
+  const username = formData.get('auth_user') as string;
+  const password = formData.get('auth_pwd') as string;
+
+  if (!username)
+    return { error: 'Please enter both username and password' };
+  else if (!password)
+    return { error: 'Please enter password' };
+
+  try {
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(eq(users.login, username.toLowerCase()));
+    
+    if (!user)
+      return { error: 'Authentication Failed' };
+
+    const isPwdValid = await bcrypt.compare(password, user.password);
+
+    if (!isPwdValid)
+      return { error: 'Authentication Failed' };
+
+    await createSession({
+      userId: user.id,
+      publicId: user.uuid,
+      role: user.role,
+    });
+  } catch (err) {
+    if (
+      err instanceof Error
+      && err.message.includes('NEXT_REDIRECT')
+    ) throw err;
+    
+    console.error('Auth error: ', err);
+
+    return { error: 'Unexpected error occurred' };
+  }
+
+  redirect('/');
+};
